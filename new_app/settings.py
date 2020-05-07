dc43dbd5da197299ac9eefa923573751360b9477
Hunter Hargues <Hunter.Hargues@flywheelenergy.com>
2020-05-06 21:52:25 -0500

    push

--- new_app/settings.py
+++ new_app/settings.py
@@ -125,8 +125,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT= os.path.join(BASE_DIR,'static-files')
+STATIC_ROOT = os.path.join(BASE_DIR,'static-files')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(STATIC_ROOT, '..', 'static'),
 ]
