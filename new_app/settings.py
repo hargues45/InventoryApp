d351862bc0c6f8cff503d0a1cd154db7dedd3418
Hunter Hargues <Hunter.Hargues@flywheelenergy.com>
2020-05-13 15:39:30 -0500

    push

--- new_app/settings.py
+++ new_app/settings.py
@@ -86,8 +86,6 @@ DATABASES = {
         'HOST': 'sql-feapps-01.flywheelenergy.com',
         'PORT': '1433',
         'OPTIONS': {
-            # 'driver': "SQL Server Native Client 11.0"
-            # 'driver': 'ODBC Driver 13 for SQL Server',
             'driver': 'ODBC Driver 17 for SQL Server'
         },
     }
